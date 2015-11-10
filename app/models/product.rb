2745e2dfedf9f16735275992e4cb1abb8bf3b325
hbqy90 <hbqy90@gmail.com>
2015-11-11 00:45:00 +0800

    Validation!

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,13 @@
 class Product < ActiveRecord::Base
+  #can't be empty
+  validates :title, :description, :image_url, :presence => true
+  #price must greater or equal to 0.01, db only keep 2 degital,don't set 0.001
+  validates :price, :numericality => {:greater_than_or_equal_to => 0.01}
+  #title must be unique
+  validates :title, :uniqueness => true
+  #validate images,must be end with jpg,png,gif
+  validates :image_url, :format => {
+    :with => %r{\.(gif|jpg|png)\z}i,
+    :message => 'must be a URL for GIF, JPG or PNG image.'
+  }
 end
